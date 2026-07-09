658afe35aa3d57f8b113453ed7e3938c7c4b7fdd
Testerzopa <testerzopa111@gmail.com>
2026-07-09 10:00:07 +0700

    I make backend server it can post data only

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
-const cors = require('cores');
+const cors = require('cors');
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('สวัสดีครับ! เว็บไซต์แรกของคุณทำงานได้แล้ว');
-});
+app.use(express.json())
+app.use(cors())
+
+let saveData = { name:"",age:"" };
+
+app.post('/', (req,res)=>{
+    saveData.name = req.body.name;
+    saveData.age = req.body.age;
+    res.json({message:"เก็บข้อมูลเรียบร้อยแล้ว"})
+})
+
+app.get('/',(req,res)=>{
+   res.json({
+    name:saveData.name,
+    age:saveData.age
+   })
+})
 
 // 5. สั่งให้เซิร์ฟเวอร์เริ่มทำงานและรอรับผู้ใช้งาน
 app.listen(PORT, () => {
---
+++ frontend/main.js
@@ -0,0 +1,24 @@
+async function addMember() {
+    const inputName = document.getElementById("input-name").value
+    const inputAge = document.getElementById("input-age").value
+    const response = await fetch("http://localhost:3000/",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            name:inputName,
+            age:inputAge,
+        })
+    })
+    const data = await response.json()
+}
+
+async function loadMember() {
+    const response = await fetch("http://localhost:3000/")
+    const user = await response.json()
+    const userUI = document.getElementById("listUser")
+    userUI.innerText = "";
+    user.forEach(user => {
+        let li = document.createElement("li")
+        li.textContent = `ชื่อ: ${user.name} (${user.name})`;
+    });
+}
